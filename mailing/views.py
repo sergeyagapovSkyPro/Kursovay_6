fd9d9073d858aae4c08d4516f75a321e4b1d40da
Segrey Agapov <stigrov@ya.ru>
2024-07-18 23:09:54 +0300

    добавил views.py

--- mailing/views.py
+++ mailing/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
 
-# Create your views here.
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView, DetailView
+
+from mailing.models import Message, MailingSettings
+
+
+class MailingMessageCreateView(CreateView):
+    model = Message
+    fields = ['title', 'content']
+    success_url = reverse_lazy('mailing:list')
+
+
+class MailingMessageUpdateView(UpdateView):
+    model = Message
+    fields = ['title', 'content']
+    success_url = reverse_lazy('mailing:list')
+
+
+class MailingMessageDeleteView(DeleteView):
+    model = Message
+    success_url = reverse_lazy('mailing:list')
+
+
+class MailingMessageListView(ListView):
+    model = Message
+
+
+class MailingMessageDetailView(DetailView):
+    model = Message
+
+
+class MailingSettingsCreateView(CreateView):
+    model = MailingSettings
+    form_class = MailingSettingsForm
+    success_url = reverse_lazy('mailing:settings_list')
+
+
+class MailingSettingsUpdateView(LoginRequiredMixin, UpdateView):
+    model = MailingSettings
+    form_class = MailingSettingsForm
+    success_url = reverse_lazy('mailing:settings_list')
+
+
+class MailingSettingsListView(LoginRequiredMixin, ListView):
+    model = MailingSettings
+
+
+class MailingSettingsDetailView(LoginRequiredMixin, DetailView):
+    model = MailingSettings
+
+
+class MailingSettingsDeleteView(LoginRequiredMixin, DeleteView):
+    model = MailingSettings
+    success_url = reverse_lazy('mailing:settings_list')
